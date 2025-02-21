1df7934f07459a39cde3fea8abaea9ba9d344d2b
w1n73rr <dushakov2005@gmail.com>
2025-02-21 15:11:15 +0400

    Add components

---
+++ yancept-frontend/src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h1>Личный кабинет Самарского университета</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
